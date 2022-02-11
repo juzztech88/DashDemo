758d1c0c7f98075c6c049c547c665a22565b4415
juzztech88 <juzztech88@users.noreply.github.com>
2022-02-11 17:22:41 +0800

    Adding test 3 in graphical

--- app_graph.py
+++ app_graph.py
@@ -3,12 +3,14 @@ import json
 from dash import Dash, Input, Output, State
 from dash import html, dcc
 import pandas as pd
+import numpy as np
 import plotly.express as px
 
 '''
 testNo --> demo concept
 1 --> hoverData
 2 --> overall
+3 --> Generic Crossfilter Recipe
 '''
 testNo = 2
 
@@ -243,6 +245,72 @@ elif testNo == 2:
     )
     def display_relayout_data(relayoutData):
         return json.dumps(relayoutData, indent=2)
+
+elif testNo == 3:
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+    app = Dash(__name__, external_stylesheets=external_stylesheets)
+
+    #   make a sample data frame with 6 columns
+    np.random.seed(0)   #   no-display
+    df = pd.DataFrame({"Col " + str(i+1) : np.random.rand(30) for i in range(6)})
+
+    #   configure app layout
+    app.layout = html.Div([
+        html.Div(
+            dcc.Graph(id='g1', config={'displayModeBar' :  False}),
+            className='four columns'
+        ),
+
+        html.Div(
+            dcc.Graph(id='g2', config={'displayModeBar' : False}),
+            className='four columns'
+        ),
+        html.Div(
+            dcc.Graph(id='g3', config={'displayModeBar' : False}),
+            classNAme='four columns'
+        )
+    ],
+    className='row')
+
+def get_figure(df, x_col, y_col, selectedpoints, selectedpoints_local):
+    if selectedpoints_local and selectedpoints_local['range']:
+        ranges = selectedpoints_local['range']
+        selection_bounds = {
+            'x0' : ranges['x'][0], 'x1' : ranges['x'][1],
+            'y0' : ranges['y'[0]], 'y1' : ranges['y'][1]
+        }
+    else:
+        selection_bounds = {
+            'x0' : np.min(df[x_col]), 'x1' : np.max(df[x_col]),
+            'y0' : np.min(df[y_col]), 'y1' : np.max(df[y_col])
+        }
+
+    #   set which points are selected with the `selectedpoints` property
+    #   and style those points with the `selected` and `unselected`
+    #   attribute. See
+    #   https://medium.com/@plotlygraphs/notes-from-the-latest-plotly-js-release-b035a5b43e21
+    #   for an explanation
+    fig = px. scatter(df, x=df[x_col], y=df[y_col], text=df.index)
+
+    fig.update_traces(selectedpoints=selectedpoints,
+                      customdata=df.index,
+                      mode='markers+text',
+                      marker={
+                          'color' : 'rgba(0, 116, 217, 0.7)',
+                          'size' : 20
+                      },
+                      unselected={
+                          'marker' : {'opacity' : 0.3},
+                          'textfont' : {'color' : 'rgba(0,0 0,0 0)'}
+                      })
+
+    fig.update_layout(
+        margin={'l':20, 'r':0, 'b':15, 't':5},
+        dragmode='select', hovermode=Fakse
+    )
+
+    fig.add_shape()
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
